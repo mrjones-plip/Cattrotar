ae0c029e85ba6499408d253aae19cc651befda3d
Ths2-9Y-LqJt6 <Ths2-9Y-LqJt6@plip.com>
2020-02-28 21:56:17 -0800

    implement volume control on chromecasts, have mute bug :(

--- cattrotar.py
+++ cattrotar.py
@@ -1,18 +1,25 @@
+print("Trying to start cattrotar...")
+print("Loading base libraries...")
+import socket, time
+from RPi import GPIO
+from time import sleep
+from Oled import Oled
+import logging, sys, os
+from Oled import Oled
+print("Loading catt lib...")
+import catt.api as cat_api
+logging.basicConfig(filename=os.path.dirname(os.path.abspath(__file__)) + "/error.log")
+try:
+    import config
+except ModuleNotFoundError as e:
+    logging.error(logging.exception(e))
+    sys.exit("ERROR: Couldn't find file 'config.py'. Did you copy and edit 'config.dist.py' per readme.md?")
+
 # todo: put these in readme.md
 # enable i2c with and install
 # follow https://learn.adafruit.com/monochrome-oled-breakouts/python-setup
 # python3 -m pip install Pillow
 # sudo apt-get install libopenjp2-7
-
-print("Trying to start cattrotar...")
-from RPi import GPIO
-from time import sleep
-from Oled import Oled
-import logging, sys, os
-# print("Libs loaded except catt lib...")
-# import catt.api as cat_api
-# print("catt lib loaded...")
-logging.basicConfig(filename=os.path.dirname(os.path.abspath(__file__)) + "/error.log")
 
 
 class cattrotar:
@@ -22,44 +29,74 @@ class cattrotar:
         self.clk = 17
         self.dt = 18
         self.sw = 23
-        self.volume = 99
+        self.volume = 1
         self.preMuteVolume = self.volume
         self.button = 0
-        print('Trying to initialize screen on default i2c bus')
+        self.last_screen_update = time.time()
+        self.cast = False
+        print('Trying to initialize screen on default i2c bus...')
         try:
             self.screen = Oled(48);
-            self.screen.display(';)')
+            self.show(';)')
             sleep(.5)
-        except Exception as e:
-            logging.error(logging.exception(e))
-            sys.exit('ERROR Could not access screen: ' + str(e))
+        except Exception as error:
+            logging.error(logging.exception(error))
+            sys.exit('ERROR Could not access screen: ' + str(error))
+
+        print('Trying to get handle to Chromecast ' + config.chromecasts[0] + '...')
+        try:
+            self.get_cast_handle(config.chromecasts[0])
+        except cat_api.CastError as error:
+            logging.error(logging.exception(error))
+            sys.exit("ERROR: Couldn't connect to '" + config.chromecasts[0] + "'. Check config.py and name/IP.")
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
         GPIO.add_event_detect(self.sw, GPIO.FALLING, callback=self.toggleMute, bouncetime=500)
 
-    def setVolume(self, volume, silent = False):
+    def get_cast_handle(self, name_or_ip):
+        try:
+            socket.inet_pton(socket.AF_INET6, name_or_ip)
+            self.cast = cat_api.CattDevice(ip_addr=name_or_ip)
+        except socket.error:
+            try:
+                socket.inet_aton(name_or_ip)
+                self.cast = cat_api.CattDevice(ip_addr=name_or_ip)
+            except socket.error:
+                self.cast = cat_api.CattDevice(name=name_or_ip)
+
+    def setVolume(self, volume, silent=False):
         if volume < 0:
             if not silent:
-                self.screen.display("MIN")
-                sleep(0.5)
-                self.screen.display(round(self.volume))
+                self.show("MIN")
+
+            self.cast.volume(int(0) / 100)
+            print("Min volume!")
+            sleep(0.5)
+            self.show(round(self.volume))
+            return 0
 
         elif volume > 100:
             if not silent:
-                self.screen.display("MAX")
-                sleep(0.5)
-                self.screen.display(round(self.volume))
+                self.show("MAX")
+            print("Max volume!")
+            sleep(0.5)
+            self.show(round(self.volume))
+            return 100
 
         else:
+            print('Setting chromecast ' + config.chromecasts[0] + ' volume to ' + str(round(self.volume)))
+            self.cast.volume(int(self.volume) / 100)
             self.volume = volume
-            print('Setting volume to ' + str(round(self.volume)))
             if not silent:
-                self.screen.display(round(self.volume))
-            # todo - actually call call catt here
+                self.show(round(self.volume))
+            return volume
+
+    def show(self, text):
+        self.screen.display(text)
+        self.last_screen_update = time.time()
 
     def main(self):
         lastVolume = self.volume
@@ -68,29 +105,12 @@ class cattrotar:
         try:
             print("Started cattrotar!");
             while True:
-                # nukeMike suggests maybe doing debouncing in software by checking
-                # the GPIO values a few times before trusting them
-                deboucnce_wait = 0.001
+
                 clkState = GPIO.input(self.clk)
                 dtState = GPIO.input(self.dt)
-                sleep(deboucnce_wait)
-                clkState2 = GPIO.input(self.clk)
-                dtState2 = GPIO.input(self.dt)
-                sleep(deboucnce_wait)
-                clkState3 = GPIO.input(self.clk)
-                dtState3 = GPIO.input(self.dt)
 
-                # see if we got a valid change
-                if clkState != clkLastState and\
-                        clkState == clkState2 and\
-                        clkState2 == clkState3 and\
-                        clkState == clkState3 and\
-                        dtState == dtState2 and\
-                        dtState2 == dtState3 and\
-                        dtState == dtState3:
-                    # reset button state to be up
-                    self.button = 0
-                    print("DEBUG start clkState: " + str(clkState) + " dtState: " + str(dtState) + " self.volume: " + str(self.volume) + " newVolume: " + str(newVolume))
+                # see if we got a change since last iteration of loop
+                if clkState != clkLastState:
                     if dtState != clkState:
                         newVolume += .5
                     else:
@@ -99,14 +119,27 @@ class cattrotar:
                     # only if new volume has changed more than .5 and it's different than prior
                     # volume levels do we actually update volume
                     if newVolume % 1 == 0 and lastVolume != newVolume:
+                        # reset button state to be up
+                        self.button = 0
+
+                        # set volume
+                        newVolume = self.setVolume(newVolume)
                         lastVolume = newVolume
-                        self.setVolume(newVolume)
+
+                # empty screen after 10 seconds
+                if (time.time() - self.last_screen_update) > 10:
+                    self.show(' ')
 
                 clkLastState = clkState
                 sleep(0.001)
+
         except KeyboardInterrupt:
             print("\nkeyboard killed the process")
+
         finally:
+            self.show('Bye!')
+            sleep(0.5)
+            self.show(' ')
             print('cattrotar exiting')
             GPIO.cleanup()
 
@@ -116,7 +149,7 @@ class cattrotar:
             self.button = 1
             self.preMuteVolume = self.volume
             self.setVolume(0, True)
-            self.screen.display("MUTE")
+            self.show("MUTE")
         else:
             print("DEBUG toggleMute UNMUTE!: " + str(self.button))
             self.button = 0
